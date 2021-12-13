af07e5609f2eb3f454ce9d5d8a15e0967c0b30cb
Dyder <dyder@Dyders-MacBook-Pro.local>
2021-12-13 22:28:13 +0200

    Initial commit

---
+++ compile.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+const solc = require("solc");
+
+// compile code will go here
+
+const inboxPath = path.join(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
+
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
